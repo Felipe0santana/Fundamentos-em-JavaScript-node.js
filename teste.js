b4b865b3f1a8b1bff55e61053e22c8706f3853cf
Felipe0santana <felipe50centt@hotmail.com>
2022-06-01 20:10:34 -0300

    Operadores e funções

---
+++ teste.js
@@ -0,0 +1,18 @@
+//let nota1 = 10
+//let nota2 = 6
+//let nota3 = 6.7
+//let nota4 = 8
+
+//let media = (nota1 + nota2 + nota3 + nota4)/ 4
+//console.log(media)
+
+// outra forma
+
+const notas = [10, 6.5, 8, 9]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3])
+/ notas.length
+
+console.log(media)
+
+
---
+++ truthy-falsy.js
@@ -0,0 +1,29 @@
+const usuarioLogado = true
+const contaPaga = false
+
+// truthy ou falsy
+
+//0 => false
+//1 => true 
+
+console.log( 0 == false)
+console.log("" == false)
+console.log( 1 == true)
+
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+
+
+console.log(minhaVar)
+console.log(varNull)
+
+// typeof   ,  define qual é o tipo do valor
+
+let numero = 3;
+let texto = " Alura "
+console,log(typeof numero)
+console.log(typeof texto)
+
